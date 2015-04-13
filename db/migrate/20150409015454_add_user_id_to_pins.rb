8763154b7c5d6864bc4624885d81c18ed41395b2
Taylor Burton <burton.taylor@gmail.com>
2015-04-13 16:07:10 -0400

    Destroyed user migration, trying to fix pins scaffold

--- db/migrate/20150409015454_add_user_id_to_pins.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdToPins < ActiveRecord::Migration
-  def change
-    add_column :pins, :user_id, :string
-    add_column :pins, :integer, :string
-    add_index :pins, :integer
-  end
-end
